32e73cbf4d43e842ae9ece17ac99c29e0f4d604d
Ruben Pinto <rpinto@spartaglobal.com>
2018-10-08 22:27:37 +0100

    Added square root function and input error message

--- js/index.js
+++ js/index.js
@@ -1,14 +1,12 @@
-var calculation = prompt ("Would you like regular or advanced calculation?").toLowerCase();
-// var regular ["+","-","*","/"];
-// var advanced
 var num1;
 var num2;
 var exp;
+var calculation = prompt ("Would you like regular or advanced calculation?").toLowerCase();
 
 if (calculation=="regular") {
   var regular = prompt("What type of regular calculation would you like to do? (+,-,/,*)");
-  num1 = prompt("Introduce the first number");
-  num2 = prompt("Introduce the second number");
+  num1 = parseInt(prompt("Introduce the first number"));
+  num2 = parseInt(prompt("Introduce the second number"));
   // If cycle for regular calculation
     if (regular=="+"){
       console.log(num1 + num2);
@@ -16,16 +14,25 @@ if (calculation=="regular") {
         console.log(num1 - num2);
       } else if (regular=="/") {
           console.log(num1 / num2);
-        }else {
+        }else if (regular==""*"") {
             console.log(num1 * num2);
+          }else {
+            //Message shown if input is wrong
+            console.log ("Wrong input. Try again and read instructions carefully!");
           }
 //cycle for advanced calculation
-} else {
-    var advanced = prompt ("Would like to do Square root (Sroot) or Exponentiation (Exp)").toLowerCase();
-    num1 = prompt ("What number do you want to use?");
-    if (advanced=="exp") {
-      console.log(Math.pow (num1, 2));
-    }else {
-        console.log (Math.sqrt(num1));
+} else if (calculation=="advanced"){
+      var advanced = prompt ("Would like to do Square root (Sroot) or Exponentiation (Exp)").toLowerCase();
+      num1 = prompt ("What number do you want to use?");
+      if (advanced=="exp") {
+        console.log(Math.pow (num1, 2));
+      }else if (advanced=="sroot"){
+          console.log (Math.sqrt(num1));
+        } else {
+          //Message shown if input is wrong
+          console.log ("Wrong input. Try again and read instructions carefully!");
+        }
+}else {
+    //Message shown if input is wrong
+    console.log ("Wrong input. Try again and read instructions carefully!");
       }
-    }
