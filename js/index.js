90694389b9e6c2fe33c2a55caa4db74604ca48bd
Ruben Pinto <rpinto@spartaglobal.com>
2018-10-08 21:27:53 +0100

    Added power funciton

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ var num1;
 var num2;
 
 if (calculation=="regular") {
-  var regular = prompt("What type of calculation would you like to do? (+,-,/,*)");
+  var regular = prompt("What type of regular calculation would you like to do? (+,-,/,*)");
   num1 = prompt("Introduce the first number");
   num2 = prompt("Introduce the second number");
   // If cycle for regular calculation
@@ -20,6 +20,12 @@ if (calculation=="regular") {
           }
 //cycle for advanced calculation
 } else {
-  var advanced
-    if ()
+    var advanced = prompt ("Would like to do Square root (Sroot) or Exponentiation (Exp)").toLowerCase();
+    num1 = prompt ("What number do you want to use?");
+    if (advanced=="exp") {
+      console.log(Math.pow(num1,2));
+      else {
+
+      }
+    }
 }
