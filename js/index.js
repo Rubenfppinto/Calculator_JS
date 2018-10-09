5a7cc11d34ea1e377f84f03ae34979f78507cfda
Ruben Pinto <rpinto@spartaglobal.com>
2018-10-09 12:16:45 +0100

    Fix the bug where it would still ask for the numbers input, even if the input for the calculations was not what it was required

--- js/index.js
+++ js/index.js
@@ -4,32 +4,39 @@ var calculation = prompt ("Would you like regular or advanced calculation?").toL
 
 if (calculation=="regular") {
   var regular = prompt("What type of regular calculation would you like to do? (+,-,/,*)");
-  num1 = parseInt(prompt("Introduce the first number"));
-  num2 = parseInt(prompt("Introduce the second number"));
   // If cycle for regular calculation
     if (regular=="+"){
-      console.log(num1 + num2);
+        num1 = parseInt(prompt("Introduce the first number"));
+        num2 = parseInt(prompt("Introduce the second number"));
+        console.log(num1 + num2);
     } else if (regular=="-") {
+        num1 = parseInt(prompt("Introduce the first number"));
+        num2 = parseInt(prompt("Introduce the second number"));
         console.log(num1 - num2);
       } else if (regular=="/") {
-            if (num2==0){
-              console.log ("Operation not possible. Cannot divide by 0")
-            } else {
-              console.log(num1 / num2);
-            }
-        }else if (regular==""*"") {
-            console.log(num1 * num2);
-          }else {
-            //Message shown if input is wrong
-            console.log ("Wrong input. Try again and read instructions carefully!");
+          num1 = parseInt(prompt("Introduce the first number"));
+          num2 = parseInt(prompt("Introduce the second number"));
+          if (num2==0){
+            console.log ("Operation not possible. Cannot divide by 0")
+          } else {
+            console.log(num1 / num2);
           }
+          }else if (regular==""*"") {
+              num1 = parseInt(prompt("Introduce the first number"));
+              num2 = parseInt(prompt("Introduce the second number"));
+              console.log(num1 * num2);
+            }else {
+            //Message shown if input is wrong
+              console.log ("Wrong input. Try again and read instructions carefully!");
+            }
 //cycle for advanced calculation
 } else if (calculation=="advanced"){
       var advanced = prompt ("Would like to do Square root (Sroot) or Exponentiation (Exp)").toLowerCase();
-      num1 = prompt ("What number do you want to use?");
       if (advanced=="exp") {
+        num1 = prompt ("What number do you want to use?");
         console.log(Math.pow (num1, 2));
       }else if (advanced=="sroot"){
+          num1 = prompt ("What number do you want to use?");
           console.log (Math.sqrt(num1));
         } else {
           //Message shown if input is wrong
